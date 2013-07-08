5ac07c84d1b7817d9ad458d60eae9b91f1434e9f
Bjoern Pahls <bjoern.pahls@gmail.com>
2013-07-08 14:46:16 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var mybuf = fs.readFileSync("index.html");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+ // response.send('Hello World2!');
+ mybuf.toString() 
 });
 
 var port = process.env.PORT || 5000;
