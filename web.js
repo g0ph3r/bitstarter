c5de85863fcae97c0168be4502d9dd9f4a4fef48
Bjoern Pahls <bjoern.pahls@gmail.com>
2013-07-08 15:08:08 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var fs =require('fs');
+var mybuf = new Buffer();
 fs.readFile('index.html', function (err, data) {
   if (err) throw err;
-  var mybuf = new Buffer(data);
+   mybuf = data;
 });
 
 var app = express.createServer(express.logger());
